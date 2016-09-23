a950a091b857830bb8da2be609d32f1cc6206ec6
somename <someemail>
2016-09-23 15:49:24 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -14,7 +14,7 @@ public class TestBowling {
 	Frame game = new Frame(0, 0);
 	game.isStrike();
 	int firstThrow = 10;
-	assertTrue(true, isStrike());
+	assertTrue(true, false, game.isStrike());
 	}
 	
 }
